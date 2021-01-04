36ec49fa8f54b5054a6a87b2ae84c0873c955d9c
Maenda Fortier <maenda@gmail.com>
2021-01-04 13:26:36 -0500

    After video 17:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in North Bay!`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // name of plugin you're installing
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '93tbjfrc',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
