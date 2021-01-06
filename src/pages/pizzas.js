1e6471ba45e9b28f51861823cc7b84df99d7922c
Maenda Fortier <maenda@gmail.com>
2021-01-06 13:21:45 -0500

    After vid 18

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import Layout from '../components/Layout';
+// Todo: vid 18 - 11:52
 
-export default function PizzasPage() {
+import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
+
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey I'm the pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
